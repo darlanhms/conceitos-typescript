cd334744f6a7e08480990be761490a72dd91c750
darlan <darlanhermessjb@hotmail.com>
2021-03-19 09:30:50 -0300

    arquivos de build

--- src/js/views/View.js
+++
@@ -1,8 +0,0 @@
-class View {
-    constructor(selector) {
-        this.element = document.querySelector(selector);
-    }
-    update(model) {
-        this.element.innerHTML = this.template(model);
-    }
-}
